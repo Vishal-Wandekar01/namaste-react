158e323430ab1cec1180f9b12fae01233e88e842
Vishal Wandekar <thevishalwandekar2000@gmail.com>
2023-12-31 18:53:53 +0530

    Done Epi-02 of Namaste React

---
+++ Episode_02_Ignite_Our_App/App.js
@@ -0,0 +1,67 @@
+/**
+ *
+ * <div id="parent">
+ *    <div id="child">
+ *       <h1>I am h1</h1>
+ *       <h1>I am h2</h1>
+ *    </div>
+ *
+ *    <div id="child2">
+ *       <h1>I am h1</h1>
+ *       <h1>I am h2</h1>
+ *    </div>
+ * </div>
+ *
+ *
+ *
+ */
+
+//only one children
+/*
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "xyz" }, "This is the nested HTML Element")
+  )
+);
+*/
+
+// more then one children...convert it into the array
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent2 = React.createElement("div", { id: "parent2" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Namaste React"),
+    React.createElement("h2", {}, "It is Just a start of React...Many more things to come"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is the nested HTML Sibling 1"),
+    React.createElement("h2", {}, "AAAAAAAAAAAAAA BBBBBBBBBBBBB CCCCCCCCCCCCCC DDDDDDDDDDDDDD"),
+  ]),
+]);
+
+//but this is very complex...the solution is the JSX...This is the last time we are using the react.createElement...This above is all core React
+/*
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World from React! Heyyyyy"
+); // we have created the react element it's nothing but the normal javascript object
+
+*/
+
+// console.log(heading); //object
+//console.log(parent);
+//console.log(parent2);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//root.render(parent);
+root.render(parent2);// render the parent2 object and put it in to the root which is available in index.html
+
---
+++ Episode_02_Ignite_Our_App/README.md
@@ -0,0 +1,26 @@
+# Namaste React
+
+# parcel
+- It create Dev Build for you
+- creates Local server for you and it host your code to server
+- HMR - Hot Module Replacement
+- It uses the file watching algorithm and this algo is written using the CPP
+- Caching - Faster Builds
+- It will do a image optimization 
+- It will do a Minification of file also
+- It will bundle all the file for US
+- It compress our file
+- It uses consistent hashing 
+- It will do code splitting for you
+- It will also do differential bundling for you - to support older browsers
+- It is doing diagnostic in behind the scene 
+- It gives you good error handling 
+- It also give a way to host our app on HTTPS
+- parceljs.org
+- It supports Tree shaking - Remove unused code for you
+- It has different Dev and Production bundles
+
+
+
+# Browserslist - 
+- https://browserslist.dev/?q=bGFzdCAyIGNocm9tZSB2ZXJzaW9ucw%3D%3D
